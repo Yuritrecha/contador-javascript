ce76018a718664fc0487178f708970bab86c9740
Yuri Trecha <116208705+beatsjazza@users.noreply.github.com>
2024-01-15 13:28:23 -0300

    Add files via upload
    Programa de contagem regressiva.

---
+++ contador.js
@@ -0,0 +1,8 @@
+alert ('Bem vindo ao seu programa de contagem regressiva')
+
+numeroContagem  = prompt('Digite um número para efetuar a contagem regressiva') 
+
+while (numeroContagem != 0){
+    alert (numeroContagem)
+    numeroContagem--
+}
